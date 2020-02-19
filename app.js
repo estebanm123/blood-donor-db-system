53a52e2c0f8a0a66553051f576b7b459ecd8b6ee
esteban0 <estebanmacaron@yahoo.fr>
2020-02-18 21:55:30 -0800

    pg test

---
+++ app.js
@@ -0,0 +1,80 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+  // next(createError(404));
+// });
+
+// app.get('*', (req, res) => {
+  // res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+  // // set locals, only providing error in development
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render('error');
+// });
+
+
+var indexRouter = require('./routes/index');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use("/api/test", indexRouter);
+
+// app.get('/api/test', (req, res) => {
+	
+	
+  // const data = "ok";
+
+  // // Return them as json
+  // res.json(data);
+// });
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+  // res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
+
+module.exports = app;
+
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const { Client } = require('pg');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+
+	const client = new Client({
+	  connectionString: process.env.DATABASE_URL.,
+	  ssl:				true,
+	});
+
+	client.connect();
+	
+	results = [];
+	client.query('select  * from ok')
+	.then( (results) => {
+		  console.log(results.rows);
+		  results = results.rows;
+		  client.end();
+		  
+		  		  res.json(JSON.stringify(results));
+
+	})
+	.catch( (err) => {
+		console.error(err);
+	});
+	
+  
+});
+
+module.exports = router;
