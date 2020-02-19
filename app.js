75f4ec4b160aec29b60c9d836563cf8ed89e88b8
esteban0 <estebanmacaron@yahoo.fr>
2020-02-18 22:13:53 -0800

    fixed a bug

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ app.use("/api/test", indexRouter);
   // res.json(data);
 // });
 
-The "catchall" handler: for any request that doesn't
-match one above, send back React's index.html file.
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
