27c8f3aded858289715162a07420a79de99c6cdb
esteban0 <estebanmacaron@yahoo.fr>
2020-02-18 22:10:45 -0800

    uncommented route handler

--- app.js
+++ app.js
@@ -61,15 +61,14 @@ app.use("/api/test", indexRouter);
 	
   // const data = "ok";
 
-  // // Return them as json
   // res.json(data);
 // });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-  // res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
+The "catchall" handler: for any request that doesn't
+match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
