3b644056e5ccdf745c68318821208e5400ac05d9
cth0604 <57742558+cth0604@users.noreply.github.com>
2020-03-28 16:20:16 -0700

    hey guys

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,8 +16,8 @@ const styles = makeStyles(theme => ({
 const App = () => {
 
 	const classes = styles();
-	const [isLoggedIn, setIsLoggedIn] = useState(true); // set to FALSE
-	const [curUser, setCurUser] = useState({userType: 'Nurse'}); // set to empty obj //userType: 'Nurse'
+	const [isLoggedIn, setIsLoggedIn] = useState(false); // set to FALSE
+	const [curUser, setCurUser] = useState({userType: ''}); // set to empty obj //userType: 'Nurse'
 
 
 	const handleLogin = (user) => {
@@ -36,7 +36,7 @@ const App = () => {
 	let view;
 	switch (curUser.userType) {
 		case ('Nurse'):
-			view = <NurseView handleLogout={handleLogout} name={name} id={"200"}/>; //set to curUser.id
+			view = <NurseView handleLogout={handleLogout} name={name} id={curUser.id}/>; //set to curUser.id
 			break;
 		case ('Admin'):
 			view = <AdminView handleLogout={handleLogout} name={name}/>;
